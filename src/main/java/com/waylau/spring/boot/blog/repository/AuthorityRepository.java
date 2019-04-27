d243a91f7c7642366b1a42587ea017e8a39ad1da
cyy7645 <chenyuyao2017@gmail.com>
2019-04-26 22:55:12 -0400

    first-version

---
+++ src/main/java/com/waylau/spring/boot/blog/repository/AuthorityRepository.java
@@ -0,0 +1,6 @@
+package com.waylau.spring.boot.blog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+}
